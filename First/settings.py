54f8f1045e0bee8760e1085922e8c66bcecbdca1
01rashid <r2115rashidd@gmail.com>
2022-07-04 19:14:54 +0500

    brnma

--- First/settings.py
+++ First/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'userprofile',
     'new',
     'extra',
+    
 ]
 
 MIDDLEWARE = [
--- extra/migrations/0001_initial.py
+++ extra/migrations/0001_initial.py
@@ -15,8 +15,8 @@ class Migration(migrations.Migration):
             name='Carusel',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('titel', models.CharField(max_length=200, verbose_name='Nomi')),
-                ('image', models.ImageField(upload_to='carusel/', verbose_name='Rasim')),
+                ('title', models.CharField(max_length=200, verbose_name='Nomi')),
+                ('image', models.ImageField(upload_to='carusel/', verbose_name='Rasm')),
             ],
             options={
                 'verbose_name': 'Carusel',
